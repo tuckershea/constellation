1204038668fcf7a0c30aba9c27f479c3324029c3
NoRePercussions <tucker@tuckershea.com>
2024-03-30 20:05:23 -0400

    Patch docker container internet

--- hosts/marlon/docker.nix
+++ hosts/marlon/docker.nix
@@ -1,5 +1,15 @@
+{ pkgs, ... }:
 {
   virtualisation.docker.enable = true;
+
+  # Patch docker container networking
+  # by giving them a specific DNS target.
+  # Weirdly enough, it seems to work correctly
+  # with tailscale routing (e.g. accessing marlon:9090),
+  # but I should make sure that it keeps working.
+  virtualisation.docker.daemon.settings = {
+    dns = [ "8.8.8.8" ];
+  };
 
   users.users.tuckershea.extraGroups = [ "docker" ];
 }
