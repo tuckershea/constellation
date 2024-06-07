995a417cf194c6483a642a7515b5c1e9431c1001
NoRePercussions <tucker@tuckershea.com>
2024-06-06 22:20:24 -0400

    Assorted Fixes

--- flake.nix
+++ flake.nix
@@ -17,7 +17,6 @@
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
 
     impermanence.url = "github:nix-community/impermanence";
-    impermanence.inputs.nixpkgs.follows = "nixpkgs";
 
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
