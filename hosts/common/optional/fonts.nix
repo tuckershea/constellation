0fdc24c2ff963e2a77dc06db86b6e8d63112ca80
NoRePercussions <tucker@tuckershea.com>
2024-07-03 18:35:58 -0400

    Update Darwin Fonts

--- hosts/common/optional/fonts.nix
+++ hosts/common/optional/fonts.nix
@@ -1,5 +1,5 @@
-{pkgs, ...}: {
-  fonts.fontDir.enable = true;
+{pkgs, lib, ...}: {
+  fonts.fontDir.enable = lib.mkIf pkgs.stdenv.isLinux true;
   fonts.packages = with pkgs; [
     recursive
     (nerdfonts.override {fonts = ["JetBrainsMono"];})
