88a88f3f3ed270cd2cb4bcbae0cfcb0d05c6b073
Tucker Shea <tucker@tuckershea.com>
2024-07-03 12:23:57 -0400

    Change name to Constellation (#33)

--- hosts/common/optional/auto-upgrade.nix
+++ hosts/common/optional/auto-upgrade.nix
@@ -1,7 +1,7 @@
 {
   system.autoUpgrade = {
     enable = true;
-    flake = "github:tuckershea/nix-config";
+    flake = "github:tuckershea/constellation";
     dates = "04:00";
     randomizedDelaySec = "45min";
     allowReboot = true;
