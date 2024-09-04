2694abc6ce12dcb40c63558f7e1297f8813d23a1
Tucker Shea <tucker@tuckershea.com>
2024-09-04 15:22:31 -0400

    Add cachix substitutors (#62)
    Builds are cached on cachix by github actions. By using it as
    a substitutor (and trusting its public key) we can utilize these
    caches.

--- hosts/common/core/nix.nix
+++ hosts/common/core/nix.nix
@@ -15,6 +15,16 @@ lib.mkMerge [
 
         # Maybe make this darwin-specific?
         extra-platforms = ["x86_64-darwin" "aarch64-darwin"];
+
+        trusted-public-keys = [
+          "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
+          "tuckershea.cachix.org-1:a9DdtLF8DyqAHFV7VHlA7YvasP6wUMHdOygVyks3JGM="
+        ];
+
+        substituters = [
+          "https://cache.nixos.org/"
+          "https://tuckershea.cachix.org"
+        ];
       };
 
       gc = {
