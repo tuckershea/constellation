178a9b0fb97c245c6a0a33446336e591ec1ce5c4
Tucker Shea <tucker@tuckershea.com>
2024-11-12 21:52:56 -0500

    fix: move from auto-optimise-store to nix.optimise (#124)
    auto-optimise-store is known to cause store corruption
    on darwin. optimise is a separate incarnation does that not have
    these issues.

--- hosts/common/darwin/nix.nix
+++ hosts/common/darwin/nix.nix
@@ -2,6 +2,12 @@
   nix.settings.extra-platforms = ["x86_64-darwin" "aarch64-darwin"];
   services.nix-daemon.enable = true;
 
+  nix.optimise.interval = [{
+    Hour = 4;
+    Minute = 15;
+    Weekday = 1;
+  }];
+
   nix.gc.interval = [{
     Hour = 3;
     Minute = 15;
