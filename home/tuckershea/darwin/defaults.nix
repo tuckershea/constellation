a98943f216fc7d175934a67d487cea49e9409269
NoRePercussions <tucker@tuckershea.com>
2024-12-12 18:28:09 -0500

    feat: update to current defaults

--- home/tuckershea/darwin/defaults.nix
+++ home/tuckershea/darwin/defaults.nix
@@ -1,14 +1,18 @@
 # System and app configuration via `defaults`
+# Large inspiration from https://medium.com/@zmre/nix-darwin-quick-tip-activate-your-preferences-f69942a93236
 {
   targets.darwin.defaults = {
-    "com.apple.TextEdit" = {
-      AddExtensionToNewPlainTextFiles = false;
-      RichText = false;
-      CheckSpellingAsYouTypeEnabledInRichTextOnly = true;
+    NSGlobalDomain = {
+      # Add a context menu item for showing the Web Inspector in web views
+      WebKitDeveloperExtras = true;
     };
-
+    "com.apple.desktopservices" = {
+      # Avoid creating .DS_Store files on network or USB volumes
+      DSDontWriteNetworkStores = true;
+      DSDontWriteUSBStores = true;
+    };
     "com.apple.dock" = {
-      autohide = false;
+      autohide = true;
       mineffect = "suck";
       orientation = "bottom";
       show-recent-count = 1;
@@ -20,6 +24,38 @@
       wvous-br-corner = 14;
       wvous-tl-corner = 5;
       wvous-tr-corner = 2;
+    };
+    "com.apple.finder" = {
+      ShowExternalHardDrivesOnDesktop = true;
+      ShowHardDrivesOnDesktop = true;
+      ShowMountedServersOnDesktop = true;
+      ShowRemovableMediaOnDesktop = true;
+      _FXSortFoldersFirst = true;
+      # When performing a search, search the current folder by default
+      FXDefaultSearchScope = "SCcf";
+    };
+    "com.apple.screencapture" = {
+      location = "~/Pictures/Screenshots";
+      type = "png";
+    };
+    "com.apple.screensaver" = {
+      # Require password immediately after sleep or screen saver begins
+      askForPassword = 1;
+      askForPasswordDelay = 0;
+    };
+    "com.apple.SoftwareUpdate" = {
+      AutomaticCheckEnabled = true;
+      # Check for software updates daily, not just once per week
+      ScheduleFrequency = 1;
+      # Download newly available updates in background
+      AutomaticDownload = 1;
+      # Install System data files & security updates
+      CriticalUpdateInstall = 1;
+    };
+    "com.apple.TextEdit" = {
+      AddExtensionToNewPlainTextFiles = false;
+      RichText = false;
+      CheckSpellingAsYouTypeEnabledInRichTextOnly = true;
     };
   };
 }
