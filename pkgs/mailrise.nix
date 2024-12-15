874c5995bb2cd0ea0ec71df46ea721123f3e4b63
Tucker Shea <tucker@tuckershea.com>
2024-12-15 14:54:07 -0500

    feat: add mailrise for notifications across lab (#144)

---
+++ pkgs/mailrise.nix
@@ -0,0 +1,22 @@
+{
+  pkgs,
+  ...
+}:
+pkgs.python3Packages.buildPythonApplication rec {
+  pname = "mailrise";
+  version = "1.4.0";
+
+  src = pkgs.fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-BKl5g4R9L5IrygMd9Vbi20iF2APpxSSfKxU25naPGTc=";
+  };
+
+  dependencies = with pkgs.python3Packages; [
+    apprise
+    aiosmtpd
+    pyyaml
+  ];
+
+
+  pythonImportsCheck = [ "mailrise" ];
+}
