15315b758c70296f1f40d8a80e0e2306081218c2
Tucker Shea <tucker@tuckershea.com>
2024-12-22 13:53:52 -0500

    experiment with stateful tailscale config (#157)

--- hosts/marlon/default.nix
+++ hosts/marlon/default.nix
@@ -1,4 +1,5 @@
 {
+  config,
   modulesPath,
   lib,
   pkgs,
@@ -31,7 +32,34 @@
 
   users.mutableUsers = false;
 
-  services.tailscaleAutoconnect.ephemeral = true;
+  services.tailscaleAutoconnect.enable = lib.mkForce false;
+  services.tailscale = {
+    enable = true;
+    openFirewall = true;
+    useRoutingFeatures = "server";
+    authKeyFile = config.sops.secrets.tailscale_key.path;
+
+    # Reconfigure everything on restart to fake declarativeness
+    # Note: we don't use extraUpFlags --reset because
+    # tailscale up is only re-run when the machine is not connected
+    # to the tailnet, not on every configuration change.
+    extraSetFlags = [
+      "--accept-dns"
+      "--accept-risk=all"
+      "--accept-routes"
+      "--advertise-exit-node"
+      "--advertise-routes="
+      "--exit-node="
+      "--exit-node-allow-lan-access=false"
+      "--hostname=${config.networking.hostName}-${config.networking.hostId}"
+      "--shields-up=false"
+      "--ssh=false"
+      "--webclient=false"
+    ];
+    extraUpFlags = [
+      "--timeout=30s"
+    ];
+  };
 
   environment.persistence."/persist" = {
     hideMounts = true;
