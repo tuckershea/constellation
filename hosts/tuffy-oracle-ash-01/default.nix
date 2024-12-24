928eb9f7d5c219ae3c1c484bf3ddb461c894b45a
NoRePercussions <tucker@tuckershea.com>
2024-12-24 18:25:55 -0500

    fix: correct duplicate persistence definition

--- hosts/tuffy-oracle-ash-01/default.nix
+++ hosts/tuffy-oracle-ash-01/default.nix
@@ -25,22 +25,6 @@
 
   users.mutableUsers = false;
 
-  environment.persistence."/persist" = {
-    hideMounts = true;
-    files = [
-      "/etc/machine-id"
-      "/etc/ssh/ssh_host_rsa_key"
-      "/etc/ssh/ssh_host_rsa_key.pub"
-      "/etc/ssh/ssh_host_ed25519_key"
-      "/etc/ssh/ssh_host_ed25519_key.pub"
-    ];
-
-    directories = [
-      "/var/log"
-      "/var/lib/nixos"  # preserve uids/gids between reboots
-    ];
-  };
-
   services.tailscaleAutoconnect.enable = lib.mkForce false;
   services.tailscale = {
     enable = true;
