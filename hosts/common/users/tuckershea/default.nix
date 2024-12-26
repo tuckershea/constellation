ff3f6727de7e1f0ed5af578c88232f52dea0fb2c
Tucker Shea <tucker@tuckershea.com>
2024-12-26 14:56:48 -0500

    Restrict SSH and nix daemon access to explicitly designated users (#166)

--- hosts/common/users/tuckershea/default.nix
+++ hosts/common/users/tuckershea/default.nix
@@ -21,7 +21,7 @@ in {
     // lib.optionalAttrs stdenv.isLinux {
       group = "tuckershea";
       isNormalUser = true;
-      extraGroups = [ "wheel" ];
+      extraGroups = [ "wheel" "nixusers" "sshusers" ];
     };
 
   users.groups.tuckershea = {};
