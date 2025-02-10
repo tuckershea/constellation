d9333887f30ccc652deaec682327bcab910ac50a
Tucker Shea <tucker@tuckershea.com>
2025-02-10 17:38:53 -0500

    atuin: zsh, daemon, misc config
    Importantly, disable auto-update (provided by nixpkgs) and set up
    the daemon to keep terminal prompts from slowing way down on init.
    
    Also disable fzf zsh integrations, so that we get ^r from atuin.

--- home/tuckershea/shell/atuin/default.nix
+++ home/tuckershea/shell/atuin/default.nix
@@ -1,16 +1,37 @@
 {
   config,
+  lib,
   ...
 }:
+let
+  cfg = config.programs.atuin;
+  flagsStr = lib.escapeShellArgs cfg.flags;
+in
 {
   programs.atuin = {
     enable = true;
-    enableZshIntegration = true;
+
+    daemon.enable = true;
+
+    flags = [
+      "--disable-up-arrow"
+    ];
 
     settings = {
       key_path = config.sops.secrets.atuin_key.path;
+
+      update_check = false;
+      sync_frequency = "5m";
+      workspace = true;
+      enter_accept = true;
+      keymap_mode = "vim-insert";
     };
   };
+
+  # compatibility with zsh-vi-mode
+  programs.zsh.initExtra = ''
+    zvm_after_init_commands+=('eval "$(${cfg.package}/bin/atuin init zsh ${flagsStr})"')
+  '';
 
   sops.secrets.atuin_key = {
     sopsFile = ./atuin_key.+tuckershea.enc;
