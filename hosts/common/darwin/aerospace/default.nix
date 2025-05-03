513aa62acc804b3efc0d86916b0ed2158ac4b901
Tucker Shea <tucker@tuckershea.com>
2025-05-03 13:08:38 -0400

    kanata/aerospace: mark as latency-sensitive on darwin
    The Darwin scheduler is very conscious of latency-sensitive
    processes in order to create a fluid user experience. I can mark
    processes as latency-sensitive to prioritize their scheduling.
    
    Kanata is latency-sensitive because it performs keyboard remapping.
    Every keystroke must go through it, and thus it must run as soon
    as possible.
    
    Aerospace is latency-sensitive because it does window management.
    Latency causes noticable delays in switching windows across spaces.
    
    Since making these changes I've noticed generally better responses
    across my system, though I haven't measured whether that's because
    these were effective or because I've changed some discipline as to
    how many things I am running at a time.

--- hosts/common/darwin/aerospace/default.nix
+++ hosts/common/darwin/aerospace/default.nix
@@ -1,4 +1,41 @@
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.services.aerospace;
+
+  format = pkgs.formats.toml { };
+  filterAttrsRecursive = pred: set:
+    lib.listToAttrs (
+      lib.concatMap (
+        name: let
+          v = set.${name};
+        in
+          if pred v
+          then [
+            (lib.nameValuePair name (
+              if lib.isAttrs v
+              then filterAttrsRecursive pred v
+              else if lib.isList v
+              then
+                (map (i:
+                  if lib.isAttrs i
+                  then filterAttrsRecursive pred i
+                  else i) (lib.filter pred v))
+              else v
+            ))
+          ]
+          else []
+      ) (lib.attrNames set)
+    );
+  filterNulls = filterAttrsRecursive (v: v != null);
+  configFile = format.generate "aerospace.toml" (filterNulls cfg.settings);
+in
 {
-  services.aerospace.enable = true;
-  services.aerospace.settings = (builtins.fromTOML (builtins.readFile ./aerospace.toml));
+  services.aerospace = {
+    enable = true;
+    settings = (builtins.fromTOML (builtins.readFile ./aerospace.toml));
+  };
+
+  launchd.user.agents.aerospace.command = 
+          lib.mkForce ("taskpolicy -l 5 ${cfg.package}/Applications/AeroSpace.app/Contents/MacOS/AeroSpace"
+          + (lib.optionalString (cfg.settings != { }) " --config-path ${configFile}"));
 }
