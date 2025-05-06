177143dccd10fc0aa27da1ff5a5f1922a5f63187
Tucker Shea <tucker@tuckershea.com>
2025-05-06 18:18:49 -0400

    hetzner: initial platform configuration
    Hetzner needs custom configuration for ipv6 (and potentially
    other things) going forward. Add a module to configure that.

---
+++ modules/nixos/hetzner.nix
@@ -0,0 +1,55 @@
+{
+   config,
+   lib,
+   pkgs,
+   ...
+}:
+let
+  inherit lib;
+  inherit (lib) types mkEnableOption mkIf mkOption;
+  cfg = config.platforms.hetzner;
+in {
+  options.platforms.hetzner = {
+    enable = mkEnableOption "hetzner";
+
+    ipv6.enable = mkEnableOption "IPv6";
+    ipv6.subnet = mkOption {
+      type = types.str;
+      description = ''
+        IPv6 subnet (usually a /64). Written as an IPv6 address,
+        including the subnet mask.
+      '';
+    };
+  };
+  
+  config = let 
+    # Some default differences between amd64 and arm64 on Hetzner
+    interface = if pkgs.system == "x86_64-linux" then "ens3"
+                                                 else "enp1s0";
+    subaddr   = if pkgs.system == "x86_64-linux" then "1"
+                                                 else "2";
+
+    mkAddress = {subnet, last_octet}: let
+      # this is totally fake
+      # but not sure if nix has real ipv6 handling provisions
+      start = builtins.elemAt (builtins.match "^(.*:)[^:]*" subnet) 0;
+    in
+      "${start}${last_octet}"
+    ;
+    address = mkAddress { inherit (cfg.ipv6) subnet; last_octet = subaddr; };
+    prefixLength = let
+      as_str = builtins.elemAt (builtins.match "^.*/(.*)$" cfg.ipv6.subnet) 0;
+    in lib.strings.toInt as_str;
+  in mkIf cfg.enable {
+    networking.defaultGateway6 = mkIf cfg.ipv6.enable {
+      address = "fe80::1";  # Default for Hetzner
+      inherit interface;
+    };
+    
+    networking.interfaces."${interface}".ipv6.addresses = mkIf cfg.ipv6.enable [
+      {
+        inherit address prefixLength;
+      }
+    ];
+  };
+}
