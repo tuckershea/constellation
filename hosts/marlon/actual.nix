d6794944bd7b8403a06ab4d3ffb321124fd75503
Tucker Shea <tucker@tuckershea.com>
2025-06-21 12:41:31 -0400

    actual: put behind nginx
    Set up nginx reverse proxy so I can run actual on port 443.
    
    This greatly simplified actual config.

--- hosts/marlon/actual.nix
+++ hosts/marlon/actual.nix
@@ -11,37 +11,38 @@
     isSystemUser = true;
   };
 
-  services.actual = {
-    enable = true;
-    settings = {
-      https = {
-        key = "/var/lib/acme/actual.constellation.tuckershea.com/key.pem";
-        cert = "/var/lib/acme/actual.constellation.tuckershea.com/cert.pem";
-      };
-      # Files are by default in the systemd StateDirectory
-      # /var/lib/private/actual
+  services.actual.enable = true;
+
+  services.nginx.enable = true;
+  services.nginx.virtualHosts."actual.constellation.tuckershea.com" = {
+    useACMEHost = "actual.constellation.tuckershea.com";
+    forceSSL = true;
+    locations."/" = {
+      proxyPass = let
+          port = toString config.services.actual.settings.port;
+        in "http://127.0.0.1:${port}";
+      extraConfig = ''
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header Host $host;
+      '';
     };
   };
 
-
   security.acme.certs."actual.constellation.tuckershea.com" = {
     domain = "actual.constellation.tuckershea.com";
-    group = "actual";
+    group = "nginx";
     dnsProvider = "cloudflare";
     # location of your CLOUDFLARE_DNS_API_TOKEN=[value]
     environmentFile = config.sops.secrets.cloudflare_dns_api_token.path;
-    reloadServices = [
-      "actual.service"
-    ];
+    # nginx is automatically reloaded
   };
 
-  environment.persistence."/persist" = {
-    files = [  ];
-    directories = [
-      {
-        directory = "/var/lib/private/actual";
-        mode = "0700";
-      }
-    ];
-  };
+  environment.persistence."/persist".directories = [
+    {
+      directory = "/var/lib/private/actual";
+      user = "actual";
+      group = "actual";
+      mode = "0700";
+    }
+  ];
 }
