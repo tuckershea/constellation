2d3902fc2a5dcd32486308da356701c9d1e6b373
Tucker Shea <tucker@tuckershea.com>
2025-06-21 21:14:29 -0400

    nginx: set defaults
    These seem like decent defaults. Hopefully they do not cause issues.

--- hosts/spencer/actual.nix
+++ hosts/spencer/actual.nix
@@ -21,10 +21,6 @@
       proxyPass = let
           port = toString config.services.actual.settings.port;
         in "http://127.0.0.1:${port}";
-      extraConfig = ''
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header Host $host;
-      '';
     };
   };
 
