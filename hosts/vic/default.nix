bef2a424adf98934a8b6b7bad8ecbd1288e73fcf
Tucker Shea <tucker@tuckershea.com>
2025-07-19 08:38:48 -0400

    plasma: option is moved
    services.xserver.desktopManager is now services.desktopManager.

--- hosts/vic/default.nix
+++ hosts/vic/default.nix
@@ -46,7 +46,7 @@
   services.xserver.enable = true;
 
   services.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma6.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     konsole
