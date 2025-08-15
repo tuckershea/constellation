b26637a03b226b61966251ff42853f71235955dc
Tucker Shea <tucker@tuckershea.com>
2025-08-15 11:18:11 -0400

    Switch shell to kitty
    Trying out kitty because of the graphics API.

--- home/tuckershea/graphical/default.nix
+++ home/tuckershea/graphical/default.nix
@@ -3,4 +3,26 @@
     ./alacritty.nix
     ./surfingkeys
   ];
+
+  programs.kitty = {
+    enable = true;
+    font.name = "JetBrainsMono Nerd Font Mono";
+    themeFile = "Dracula";
+    settings = {
+      update_check_interval = 0;
+      scrollback_lines = 10000;
+      enable_audio_bell = "no";
+      visual_bell_duration = "0.5 ease-in-out";
+      window_alert_on_bell = "no";
+      remember_window_size = "no";
+      remember_window_position = "no";
+      placement_strategy = "bottom-left";
+      resize_in_steps = "no";  # let window manager do it
+      confirm_os_window_close = 0;
+      macos_option_as_alt = "left";
+      macos_show_window_title_in = "menubar";
+      macos_menubar_title_max_length = 40;
+      hide_window_decorations = "yes";
+    };
+  };
 }
